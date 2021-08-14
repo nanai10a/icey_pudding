2f721b293a7fc5080bde0f05496b0a01924d3069
Nanai10a <Nanai10a@outlook.jp>
2021-08-15 03:12:17 +0900

    🚧 `Repository<Content>`の実装.

--- src/repositories.rs
+++ src/repositories.rs
@@ -1,7 +1,7 @@
 use anyhow::bail;
 use serenity::model::id::UserId;
 
-use crate::entities::User;
+use crate::entities::{Content, User};
 
 #[serenity::async_trait]
 pub trait Repository {
@@ -30,7 +30,6 @@ pub struct InMemoryRepository<T>(Mutex<Vec<T>>);
 impl<T> InMemoryRepository<T> {
     pub async fn new() -> Self { Self(Mutex::new(vec![])) }
 }
-
 #[serenity::async_trait]
 impl Repository for InMemoryRepository<User> {
     type Item = User;
@@ -75,7 +74,7 @@ impl Repository for InMemoryRepository<User> {
         let vec: &mut Vec<_> = guard.as_mut();
 
         let matched = self.get_match(queries).await?;
-        let res = try_remove_target_from_vec(vec, &matched);
+        let res = try_remove_target_from_vec(vec, &matched, |v1, v2| v1.id == v2.id);
 
         match res {
             Ok(o) => o,
@@ -92,7 +91,7 @@ impl Repository for InMemoryRepository<User> {
         let matches = self.get_matches(queries).await?;
         let res = matches
             .iter()
-            .try_for_each(|t| try_remove_target_from_vec(vec, t));
+            .try_for_each(|t| try_remove_target_from_vec(vec, t, |v1, v2| v1.id == v2.id));
 
         match res {
             Ok(o) => o,
@@ -103,11 +102,15 @@ impl Repository for InMemoryRepository<User> {
     }
 }
 
-fn try_remove_target_from_vec(vec: &mut Vec<User>, target: &User) -> anyhow::Result<()> {
+fn try_remove_target_from_vec<T>(
+    vec: &mut Vec<T>,
+    target: &T,
+    compare: impl Fn(&T, &T) -> bool,
+) -> anyhow::Result<()> {
     let mut indexes = vec
         .iter()
         .enumerate()
-        .filter_map(|(i, v)| match target.id == v.id {
+        .filter_map(|(i, v)| match compare(target, v) {
             true => Some(i),
             false => None,
         })
@@ -130,4 +133,115 @@ pub enum UserQuery {
     SubAdmin(bool),
     Posted(Vec<Uuid>),
     Bookmark(Vec<Uuid>),
+}
+
+#[serenity::async_trait]
+impl Repository for InMemoryRepository<Content> {
+    type Item = Content;
+    type Query = ContentQuery;
+
+    async fn save(&self, item: Self::Item) -> anyhow::Result<()> {
+        self.0.lock().await.push(item);
+        Ok(())
+    }
+
+    async fn get_matches(&self, mut queries: Vec<Self::Query>) -> anyhow::Result<Vec<Self::Item>> {
+        let locked = self.0.lock().await;
+        let mut vec = locked.iter().collect::<Vec<_>>();
+
+        let res = queries
+            .drain(..)
+            .try_for_each::<_, anyhow::Result<()>>(|q| {
+                match q {
+                    ContentQuery::Id(i) => vec = vec.drain(..).filter(|v| v.id == i).collect(),
+                    ContentQuery::Content(s) => {
+                        let r = regex::Regex::new(s.as_str())?;
+                        vec = vec
+                            .drain(..)
+                            .filter(|v| r.is_match(v.content.as_str()))
+                            .collect();
+                    },
+
+                    ContentQuery::Liked(l) => {
+                        vec = vec
+                            .drain(..)
+                            .filter(|v| l.iter().filter(|i| v.liked.contains(i)).count() == l.len())
+                            .collect();
+                    },
+                    ContentQuery::Bookmarked(b, c) => {
+                        vec = vec
+                            .drain(..)
+                            .filter(|v| match c {
+                                Comparison::Over => v.bookmarked >= b,
+                                Comparison::Eq => v.bookmarked == b,
+                                Comparison::Under => v.bookmarked <= b,
+                            })
+                            .collect();
+                    },
+                    ContentQuery::Pinned(p) => {
+                        vec = vec
+                            .drain(..)
+                            .filter(|v| {
+                                p.iter().filter(|i| v.pinned.contains(i)).count() == p.len()
+                            })
+                            .collect();
+                    },
+                }
+
+                Ok(())
+            });
+
+        match res {
+            Ok(o) => o,
+            Err(e) => bail!("{}", e),
+        }
+
+        Ok(vec.drain(..).cloned().collect())
+    }
+
+    async fn remove_match(&self, queries: Vec<Self::Query>) -> anyhow::Result<Self::Item> {
+        let mut guard = self.0.lock().await;
+        let vec: &mut Vec<_> = guard.as_mut();
+
+        let matched = self.get_match(queries).await?;
+        let res = try_remove_target_from_vec(vec, &matched, |v1, v2| v1.id == v2.id);
+
+        match res {
+            Ok(o) => o,
+            Err(e) => bail!("{}", e),
+        }
+
+        Ok(matched)
+    }
+
+    async fn remove_matches(&self, queries: Vec<Self::Query>) -> anyhow::Result<Vec<Self::Item>> {
+        let mut guard = self.0.lock().await;
+        let vec: &mut Vec<_> = guard.as_mut();
+
+        let matches = self.get_matches(queries).await?;
+        let res = matches
+            .iter()
+            .try_for_each(|t| try_remove_target_from_vec(vec, t, |v1, v2| v1.id == v2.id));
+
+        match res {
+            Ok(o) => o,
+            Err(e) => bail!("{}", e),
+        }
+
+        Ok(matches)
+    }
+}
+
+pub enum ContentQuery {
+    Id(Uuid),
+    Content(String),
+    Liked(Vec<UserId>),
+    Bookmarked(u32, Comparison),
+    Pinned(Vec<UserId>),
+}
+
+pub enum Comparison {
+    Over,
+    Eq,
+    Under,
 }
