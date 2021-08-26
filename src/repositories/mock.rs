dc62ecc8d79f52c444a1fc37fac2fa76bdfcf90a
Nanai10a <Nanai10a@outlook.jp>
2021-08-26 16:33:52 +0900

    🚚 mockと他をmodule分けしました.

---
+++ src/repositories/mock.rs
@@ -0,0 +1,96 @@
+use async_trait::async_trait;
+use tokio::sync::Mutex;
+use uuid::Uuid;
+
+use super::{RepositoryError, Result, UserMutation, UserRepository};
+use crate::entities::User;
+
+pub struct InMemoryRepository<T>(Mutex<Vec<T>>);
+
+#[async_trait]
+impl UserRepository for InMemoryRepository<User> {
+    async fn insert(&self, item: User) -> Result<()> {
+        self.0.lock().await.push(item);
+
+        Ok(())
+    }
+
+    async fn is_exists(&self, id: u64) -> Result<bool> {
+        let guard = self.0.lock().await;
+
+        match guard.iter().filter(|v| *v.id == id).count() {
+            0 => Ok(false),
+            1 => Ok(true),
+            i => Err(RepositoryError::NoUnique { matched: i }),
+        }
+    }
+
+    async fn find(&self, id: u64) -> Result<User> {
+        let guard = self.0.lock().await;
+        let res = guard.iter().filter(|v| *v.id == id).collect::<Vec<_>>();
+
+        match res.len() {
+            0 => Err(RepositoryError::NotFound),
+            1 => Ok(res.remove(0).clone()),
+            i => Err(RepositoryError::NoUnique { matched: i }),
+        }
+    }
+
+    async fn is_posted(&self, id: u64, content_id: Uuid) -> Result<bool> {
+        let item = self.find(id).await?;
+
+        match item.posted.iter().filter(|v| *v == content_id).count() {
+            0 => Ok(false),
+            1 => Ok(true),
+            i => Err(RepositoryError::NoUnique { matched: i }),
+        }
+    }
+
+    async fn is_bookmarked(&self, id: u64, content_id: Uuid) -> Result<bool> {
+        let item = self.find(id).await?;
+
+        match item.bookmark.iter().filter(|v| *v == content_id).count() {
+            0 => Ok(false),
+            1 => Ok(true),
+            i => Err(RepositoryError::NoUnique { matched: i }),
+        }
+    }
+
+    async fn update(&self, id: u64, mutation: UserMutation) -> Result<User> {
+        let guard = self.0.lock().await;
+        let res = guard.iter_mut().filter(|v| *v.id == id).collect::<Vec<_>>();
+        let item = match res.len() {
+            0 => return Err(RepositoryError::NotFound),
+            1 => res.remove(0),
+            i => return Err(RepositoryError::NoUnique { matched: i }),
+        };
+
+        let UserMutation { admin, sub_admin } = mutation;
+        if let Some(val) = admin {
+            item.admin = val;
+        }
+        if let Some(val) = sub_admin {
+            item.sub_admin = val;
+        }
+
+        Ok(item.clone())
+    }
+
+    async fn delete(&self, id: u64) -> Result<User> {
+        let guard = self.0.lock().await;
+        let res = guard
+            .iter()
+            .enumerate()
+            .filter(|(_, v)| *v.id == id)
+            .map(|(i, _)| i)
+            .collect::<Vec<_>>();
+
+        let index = match res.len() {
+            0 => return Err(RepositoryError::NotFound),
+            1 => res.remove(0),
+            i => return Err(RepositoryError::NoUnique { matched: i }),
+        };
+
+        Ok(guard.remove(index))
+    }
+}
--- src/repositories/mod.rs
+++ src/repositories/mod.rs
@@ -2,10 +2,11 @@ use async_trait::async_trait;
 use mongodb::Collection;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
-use tokio::sync::Mutex;
 use uuid::Uuid;
 
 use crate::entities::{Author, Content, User};
+
+pub mod mock;
 
 type StdResult<T, E> = ::std::result::Result<T, E>;
 type Result<T> = ::std::result::Result<T, RepositoryError>;
@@ -36,94 +37,6 @@ pub trait ContentRepository: Send + Sync + Clone + Same {
     async fn is_pinned(&self, user_id: u64) -> Result<bool>;
     async fn update(&self, mutation: ContentMutation) -> Result<Content>;
     async fn delete(&self, id: Uuid) -> Result<Content>;
-}
-
-#[async_trait]
-impl UserRepository for InMemoryRepository<User> {
-    async fn insert(&self, item: User) -> Result<()> {
-        self.0.lock().await.push(item);
-
-        Ok(())
-    }
-
-    async fn is_exists(&self, id: u64) -> Result<bool> {
-        let guard = self.0.lock().await;
-
-        match guard.iter().filter(|v| *v.id == id).count() {
-            0 => Ok(false),
-            1 => Ok(true),
-            i => Err(RepositoryError::NoUnique { matched: i }),
-        }
-    }
-
-    async fn find(&self, id: u64) -> Result<User> {
-        let guard = self.0.lock().await;
-        let res = guard.iter().filter(|v| *v.id == id).collect::<Vec<_>>();
-
-        match res.len() {
-            0 => Err(RepositoryError::NotFound),
-            1 => Ok(res.remove(0).clone()),
-            i => Err(RepositoryError::NoUnique { matched: i }),
-        }
-    }
-
-    async fn is_posted(&self, id: u64, content_id: Uuid) -> Result<bool> {
-        let item = self.find(id).await?;
-
-        match item.posted.iter().filter(|v| *v == content_id).count() {
-            0 => Ok(false),
-            1 => Ok(true),
-            i => Err(RepositoryError::NoUnique { matched: i }),
-        }
-    }
-
-    async fn is_bookmarked(&self, id: u64, content_id: Uuid) -> Result<bool> {
-        let item = self.find(id).await?;
-
-        match item.bookmark.iter().filter(|v| *v == content_id).count() {
-            0 => Ok(false),
-            1 => Ok(true),
-            i => Err(RepositoryError::NoUnique { matched: i }),
-        }
-    }
-
-    async fn update(&self, id: u64, mutation: UserMutation) -> Result<User> {
-        let guard = self.0.lock().await;
-        let res = guard.iter_mut().filter(|v| *v.id == id).collect::<Vec<_>>();
-        let item = match res.len() {
-            0 => return Err(RepositoryError::NotFound),
-            1 => res.remove(0),
-            i => return Err(RepositoryError::NoUnique { matched: i }),
-        };
-
-        let UserMutation { admin, sub_admin } = mutation;
-        if let Some(val) = admin {
-            item.admin = val;
-        }
-        if let Some(val) = sub_admin {
-            item.sub_admin = val;
-        }
-
-        Ok(item.clone())
-    }
-
-    async fn delete(&self, id: u64) -> Result<User> {
-        let guard = self.0.lock().await;
-        let res = guard
-            .iter()
-            .enumerate()
-            .filter(|(_, v)| *v.id == id)
-            .map(|(i, _)| i)
-            .collect::<Vec<_>>();
-
-        let index = match res.len() {
-            0 => return Err(RepositoryError::NotFound),
-            1 => res.remove(0),
-            i => return Err(RepositoryError::NoUnique { matched: i }),
-        };
-
-        Ok(guard.remove(index))
-    }
 }
 
 #[derive(Debug)]
@@ -168,10 +81,6 @@ pub fn try_remove_target_from_vec<T>(
     }
 }
 
-pub struct InMemoryRepository<T>(Mutex<Vec<T>>);
-
-
-
 #[derive(Debug, Clone, Default)]
 pub struct UserMutation {
     pub admin: Option<bool>,
@@ -183,5 +92,3 @@ pub struct ContentMutation {
     pub author: Option<Author>,
     pub content: Option<String>,
 }
-
-
