8e1465959bea403e6e6149bc33528be4a9607021
Nanai10a <Nanai10a@outlook.jp>
2021-08-27 05:30:29 +0900

    🚧 mongoのrepositoryへのhelper fnを実装.

--- src/repositories/mongo.rs
+++ src/repositories/mongo.rs
@@ -1,13 +1,39 @@
-use mongodb::Collection;
+use mongodb::{Collection, Database};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 pub struct MongoUserRepository {
     main_coll: Collection<MongoUserModel>,
+    db: Database,
+}
+
+impl MongoUserRepository {
+    #[inline]
+    fn posted_coll(&self, id: u64) -> Collection<Uuid> {
+        self.db.collection(format!("user:{}#posted", id).as_str())
+    }
+
+    #[inline]
+    fn bookmarked_coll(&self, id: u64) -> Collection<Uuid> {
+        self.db.collection(format!("user:{}#bookmarked", id).as_str())
+    }
 }
 
 pub struct MongoContentRepository {
     main_coll: Collection<MongoContentModel>,
+    db: Database,
+}
+
+impl MongoContentRepository {
+    #[inline]
+    fn liked_coll(&self, id: Uuid) -> Collection<u64> {
+        self.db.collection(format!("content:{}#liked", id.as_u128()).as_str())
+    }
+
+    #[inline]
+    fn pinned_coll(&self, id: Uuid) -> Collection<u64> {
+        self.db.collection(format!("content:{}#pinned", id.as_u128()).as_str())
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
