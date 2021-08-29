0408e025d49aa3f5c568f5beaa2c5381689961df
Nanai10a <Nanai10a@outlook.jp>
2021-08-29 09:43:55 +0900

    🚧 他のuserがそのuserに変更を与える…ってのは無いので, paramを削除.

--- src/conductors/mod.rs
+++ src/conductors/mod.rs
@@ -67,39 +67,29 @@ pub enum Command {
 }
 
 /// command data.
-/// *note: `(user_)?id: Option<u64>` is expecting fallback to executed user's
-/// id.*
 #[derive(Debug, Clone)]
 pub enum CommandV2 {
     /// commands about user.
     User(UserCommandV2),
     /// commands about content.
     Content(ContentCommandV2),
-    /// post content with user_id.
-    /// if not given id, fallback to executed user's id.
+    /// post content with executed user's id.
     Post {
-        user_id: Option<u64>,
         author: Author,
         content: String,
     },
-    /// (un)like content with user_id.
-    /// if not given id, fallback to executed user's id.
+    /// (un)like content with executed user's id.
     Like {
-        user_id: Option<u64>,
         content_id: Uuid,
         undo: bool,
     },
-    /// (un)pin content with user_id.
-    /// if not given id, fallback to executed user's id.
+    /// (un)pin content with executed user's id.
     Pin {
-        user_id: Option<u64>,
         content_id: Uuid,
         undo: bool,
     },
-    /// (un)bookmark content to user_id.
-    /// if not given id, fallback to executed user's id.
+    /// (un)bookmark content to executed user's id.
     Bookmark {
-        user_id: Option<u64>,
         content_id: Uuid,
         undo: bool,
     },
@@ -110,13 +100,11 @@ pub enum UserCommandV2 {
     /// create user with executed user's id.
     Create,
     /// read user with id.
+    /// if not given id, fallback to executed user's id.
     Read { id: Option<u64> },
-    Reads {
-        /// read users with query.
-        /// page must satisfies `1..`.
-        page: u32,
-        query: UserQuery,
-    },
+    /// read users with query.
+    /// page must satisfies `1..`.
+    Reads { page: u32, query: UserQuery },
     /// update user with id and mutation.
     /// it's **must** given id.
     Update { id: u64, mutation: UserMutation },
