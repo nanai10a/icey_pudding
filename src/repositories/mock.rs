b13733e91942cb3ae6752601426db68e9ad3db5c
Nanai10a <Nanai10a@outlook.jp>
2021-09-01 09:38:52 +0900

    🚧 お捜しhelper_fnを実装.

--- src/repositories/mock.rs
+++ src/repositories/mock.rs
@@ -19,6 +19,30 @@ impl<T> Default for InMemoryRepository<T> {
     fn default() -> Self { Self::new() }
 }
 
+#[inline]
+fn find_mut<T, P>(v: &mut Vec<T>, preficate: P) -> Result<&mut T>
+where P: FnMut(&&mut T) -> bool {
+    let mut res = v.iter_mut().filter(preficate).collect::<Vec<_>>();
+
+    match res.len() {
+        0 => Err(RepositoryError::NotFound),
+        1 => Ok(res.remove(0)),
+        i => Err(RepositoryError::NoUnique { matched: i as u32 }),
+    }
+}
+
+#[inline]
+fn find_ref<T, P>(v: &Vec<T>, preficate: P) -> Result<&T>
+where P: FnMut(&&T) -> bool {
+    let mut res = v.iter().filter(preficate).collect::<Vec<_>>();
+
+    match res.len() {
+        0 => Err(RepositoryError::NotFound),
+        1 => Ok(res.remove(0)),
+        i => Err(RepositoryError::NoUnique { matched: i as u32 }),
+    }
+}
+
 #[async_trait]
 impl UserRepository for InMemoryRepository<User> {
     async fn insert(&self, item: User) -> Result<bool> {
