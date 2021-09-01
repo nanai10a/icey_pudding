25e9ce30c94ebd93f57ff941c93f72b5224011b0
Nanai10a <Nanai10a@outlook.jp>
2021-09-01 14:01:43 +0900

    🚧 repo/mongoの実装の雛形だけ作成.

--- src/repositories/mongo.rs
+++ src/repositories/mongo.rs
@@ -1,6 +1,12 @@
+use anyhow::anyhow;
+use async_trait::async_trait;
+use mongodb::bson::Bson;
 use mongodb::{Collection, Database};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+
+use super::{ContentRepository, RepositoryError, Result, UserRepository};
+use crate::entities::{Author, Content, User};
 
 pub struct MongoUserRepository {
     main_coll: Collection<MongoUserModel>,
@@ -62,4 +68,66 @@ enum Author {
         nick: Option<String>,
     },
     Virtual(String),
+}
+
+#[async_trait]
+impl UserRepository for MongoUserRepository {
+    async fn insert(&self, item: User) -> Result<bool> { unimplemented!() }
+
+    async fn is_exists(&self, id: u64) -> Result<bool> { unimplemented!() }
+
+    async fn find(&self, id: u64) -> Result<User> { unimplemented!() }
+
+    async fn finds(&self, query: super::UserQuery) -> Result<Vec<User>> { unimplemented!() }
+
+    async fn update(&self, id: u64, mutation: super::UserMutation) -> Result<User> {
+        unimplemented!()
+    }
+
+    async fn is_posted(&self, id: u64, content_id: Uuid) -> Result<bool> { unimplemented!() }
+
+    async fn insert_posted(&self, id: u64, content_id: Uuid) -> Result<bool> { unimplemented!() }
+
+    async fn delete_posted(&self, id: u64, content_id: Uuid) -> Result<bool> { unimplemented!() }
+
+    async fn is_bookmarked(&self, id: u64, content_id: Uuid) -> Result<bool> { unimplemented!() }
+
+    async fn insert_bookmarked(&self, id: u64, content_id: Uuid) -> Result<bool> {
+        unimplemented!()
+    }
+
+    async fn delete_bookmarked(&self, id: u64, content_id: Uuid) -> Result<bool> {
+        unimplemented!()
+    }
+
+    async fn delete(&self, id: u64) -> Result<User> { unimplemented!() }
+}
+
+#[async_trait]
+impl ContentRepository for MongoContentRepository {
+    async fn insert(&self, item: Content) -> Result<bool> { unimplemented!() }
+
+    async fn is_exists(&self, id: Uuid) -> Result<bool> { unimplemented!() }
+
+    async fn find(&self, id: Uuid) -> Result<Content> { unimplemented!() }
+
+    async fn finds(&self, query: super::ContentQuery) -> Result<Vec<Content>> { unimplemented!() }
+
+    async fn update(&self, id: Uuid, mutation: super::ContentMutation) -> Result<Content> {
+        unimplemented!()
+    }
+
+    async fn is_liked(&self, id: Uuid, user_id: u64) -> Result<bool> { unimplemented!() }
+
+    async fn insert_liked(&self, id: Uuid, user_id: u64) -> Result<bool> { unimplemented!() }
+
+    async fn delete_liked(&self, id: Uuid, user_id: u64) -> Result<bool> { unimplemented!() }
+
+    async fn is_pinned(&self, id: Uuid, user_id: u64) -> Result<bool> { unimplemented!() }
+
+    async fn insert_pinned(&self, id: Uuid, user_id: u64) -> Result<bool> { unimplemented!() }
+
+    async fn delete_pinned(&self, id: Uuid, user_id: u64) -> Result<bool> { unimplemented!() }
+
+    async fn delete(&self, id: Uuid) -> Result<Content> { unimplemented!() }
 }
