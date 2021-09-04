73b7e2aef98d2f0d2a03e384afc16edf524e3e40
Nanai10a <Nanai10a@outlook.jp>
2021-09-05 01:48:17 +0900

    🚧 実装進捗 (8/24).

--- src/repositories/mongo.rs
+++ src/repositories/mongo.rs
@@ -392,7 +392,19 @@ impl UserRepository for MongoUserRepository {
         Ok(res.modified_count.into_bool())
     }
 
-    async fn delete_posted(&self, id: u64, content_id: Uuid) -> Result<bool> { unimplemented!() }
+    async fn delete_posted(&self, id: u64, content_id: Uuid) -> Result<bool> {
+        let res = self
+            .posted_coll
+            .update_one(
+                doc! { "id": id.to_string() },
+                doc! { "$pull": { "set": content_id.to_string() } },
+                None,
+            )
+            .await
+            .cvt()?;
+        res.matched_count.into_bool().expect_true()?;
+        Ok(res.modified_count.into_bool())
+    }
 
     async fn is_bookmarked(&self, id: u64, content_id: Uuid) -> Result<bool> { unimplemented!() }
 
