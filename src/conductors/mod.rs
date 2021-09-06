2dba236e8d45013eede2e6f51ef5989dab830911
Nanai10a <Nanai10a@outlook.jp>
2021-09-06 19:15:41 +0900

    💡 自身の`::len = 0`なmsgに毎回反応しているので, 早期returnを検討.

--- src/conductors/mod.rs
+++ src/conductors/mod.rs
@@ -421,6 +421,7 @@ impl<T> ConvertRange<T> for RangeToInclusive<T> {
 #[async_trait]
 impl EventHandler for Conductor {
     async fn message(&self, ctx: Context, msg: Message) {
+        // FIXME: don't accept msg from myself(bot).
         let parse_res = match helper::parse_msg_v2(msg.content.as_str()).await {
             Some(o) => o,
             None => return,
