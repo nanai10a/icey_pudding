5de775dbcd298d05f8e8bf4c48da04a852fb76e9
Nanai10a <Nanai10a@outlook.jp>
2021-09-10 18:55:11 +0900

    💡 FiF: can get ...

--- src/repositories/mod.rs
+++ src/repositories/mod.rs
@@ -21,6 +21,7 @@ pub trait UserRepository {
 
     async fn update(&self, id: UserId, mutation: UserMutation) -> Result<User>;
 
+    // TODO: async fn get_bookmark(&self, id: UserId) -> Result<HashSet<ContentId>>;
     async fn is_bookmark(&self, id: UserId, content_id: ContentId) -> Result<bool>;
     async fn insert_bookmark(&self, id: UserId, content_id: ContentId) -> Result<bool>;
     async fn delete_bookmark(&self, id: UserId, content_id: ContentId) -> Result<bool>;
@@ -38,10 +39,12 @@ pub trait ContentRepository {
 
     async fn update(&self, id: ContentId, mutation: ContentMutation) -> Result<Content>;
 
+    // TODO: async fn get_liked(&self, id: ContentId) -> Result<HashSet<UserId>>;
     async fn is_liked(&self, id: ContentId, user_id: UserId) -> Result<bool>;
     async fn insert_liked(&self, id: ContentId, user_id: UserId) -> Result<bool>;
     async fn delete_liked(&self, id: ContentId, user_id: UserId) -> Result<bool>;
 
+    // TODO: async fn get_pinned(&self, id: ContentId) -> Result<HashSet<UserId>>;
     async fn is_pinned(&self, id: ContentId, user_id: UserId) -> Result<bool>;
     async fn insert_pinned(&self, id: ContentId, user_id: UserId) -> Result<bool>;
     async fn delete_pinned(&self, id: ContentId, user_id: UserId) -> Result<bool>;
