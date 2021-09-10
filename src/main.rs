cf55218e5cebd2e2e080322ae3c59bf2451d9d0a
Nanai10a <Nanai10a@outlook.jp>
2021-09-11 01:21:11 +0900

    🚧 changed `main`.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,21 @@
-use std::env::{args, var};
-
-use icey_pudding::in_memory;
-use serenity::client::bridge::gateway::GatewayIntents;
-use serenity::client::ClientBuilder;
-
 async fn async_main() {
-    let AppValues { token } = match get_values() {
-        Ok(o) => o,
-        Err(e) => return e,
+    let AppValues { token, flag } = get_values();
+
+    use serenity::client::bridge::gateway::GatewayIntents;
+    let cb = ::serenity::client::ClientBuilder::new(token)
+        .intents(GatewayIntents::GUILD_MESSAGES | GatewayIntents::DIRECT_MESSAGES);
+
+    let cb = match flag {
+        Flag::InMemory => cb.event_handler(::icey_pudding::in_memory()),
+        Flag::Mongo { uri, name } =>
+            cb.event_handler(::icey_pudding::mongo(uri, name).await.expect("eh error")),
     };
 
-    let mut c = match ClientBuilder::new(token)
-        .intents(GatewayIntents::GUILD_MESSAGES | GatewayIntents::DIRECT_MESSAGES)
-        .event_handler(in_memory())
+    let mut c = cb.await.expect("cannot build serenity client.");
+
+    c.start_autosharded()
         .await
-    {
-        Ok(c) => c,
-        Err(e) => return eprintln!("cannot build serenity `Client`: {}", e),
-    };
-
-    match c.start_autosharded().await {
-        Ok(o) => o,
-        Err(e) => eprintln!("serenity `Client` returned: {}", e),
-    }
+        .expect("serenity client returned.");
 }
 
 fn main() {
@@ -46,38 +39,29 @@ static mut NUM: u32 = 0;
 
 struct AppValues {
     token: String,
+    flag: Flag,
 }
 
-fn get_values() -> Result<AppValues, ()> {
-    let mut args = args();
-    args.next(); // 最初の引数はcli上のcommand_name
-
-    let token =
-        crate::try_get_value!(args; "DISCORD_BOT_TOKEN", "BUILD_WITH_DISCORD_BOT_TOKEN", "token")?;
-
-    Ok(AppValues { token })
+enum Flag {
+    InMemory,
+    Mongo { uri: String, name: String },
 }
 
-#[macro_export]
-macro_rules! try_get_value {
-    ($a:expr; $n:literal, $bn:literal, $pn:literal) => {{
-        match $a.next() {
-            Some(t) => Ok(t),
-            None => match var($n) {
-                Ok(t) => Ok(t),
-                Err(e) => {
-                    eprintln!("error on getting `{}`: {}", $n, e);
-                    eprintln!("fallback to built-in `{}`...", stringify!($pn));
+fn get_values() -> AppValues {
+    use std::env::var;
 
-                    match option_env!($bn) {
-                        Some(t) => Ok(t.to_string()),
-                        None => {
-                            eprintln!("cannot get `{}`!", stringify!($pn));
-                            Err(())
-                        },
-                    }
-                },
-            },
-        }
-    }};
+    let token = var("DISCORD_BOT_TOKEN").expect("error on: DISCORD_BOT_TOKEN");
+
+    let flag = match var("FLAG").expect("error on: FLAG").as_str() {
+        "InMemory" => Flag::InMemory,
+        "Mongo" => {
+            let uri = var("MONGO_URI").expect("error on: MONGO_URI");
+            let name = var("MONGO_DB_NAME").expect("error on: MONGO_DB_NAME");
+
+            Flag::Mongo { uri, name }
+        },
+        v => panic!("unexpected value: {}", v),
+    };
+
+    AppValues { token, flag }
 }
