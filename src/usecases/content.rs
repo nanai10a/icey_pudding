b504f09caa4e61d09bcefa1f895300ddb5d58f73
Nanai10a <Nanai10a@outlook.jp>
2021-09-12 15:54:49 +0900

    🚧 defined usecases for content.

--- src/usecases/content.rs
+++ src/usecases/content.rs
@@ -1,1 +1,95 @@
+usecase! {
+    post : {
+        content: String,
+        posted: entities::Posted,
+        author: entities::Author,
+        created: entities::Date,
+    } => {
+        content: entities::Content,
+    }
+}
 
+usecase! {
+    get : {
+        content_id: entities::ContentId,
+    } => {
+        content: entities::Content,
+    }
+}
+
+usecase! {
+    gets : {
+        query: crate::repositories::ContentQuery,
+    } => {
+        contents: Vec<entities::Content>,
+    }
+}
+
+usecase! {
+    edit : {
+        content_id: entities::ContentId,
+        mutation: crate::repositories::ContentMutation,
+    } => {
+        content: entities::Content,
+    }
+}
+
+usecase! {
+    withdraw : {
+        content_id: entities::ContentId,
+    } => {
+        content: entities::Content,
+    }
+}
+
+usecase! {
+    get_like : {
+        content_id: entities::ContentId,
+    } => {
+        like: std::collections::HashSet<entities::UserId>,
+    }
+}
+
+usecase! {
+    like : {
+        content_id: entities::ContentId,
+        user_id: entities::UserId,
+    } => {
+        content: entities::Content
+    }
+}
+
+usecase! {
+    unlike : {
+        content_id: entities::ContentId,
+        user_id: entities::UserId,
+    } => {
+        content: entities::Content
+    }
+}
+
+usecase! {
+    get_pin : {
+        content_id: entities::ContentId,
+    } => {
+        like: std::collections::HashSet<entities::UserId>,
+    }
+}
+
+usecase! {
+    pin : {
+        content_id: entities::ContentId,
+        user_id: entities::UserId,
+    } => {
+        content: entities::Content
+    }
+}
+
+usecase! {
+    unpin : {
+        content_id: entities::ContentId,
+        user_id: entities::UserId,
+    } => {
+        content: entities::Content
+    }
+}
