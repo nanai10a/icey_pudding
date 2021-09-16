1aeaad3a67b306dd4fd15c743616477e7a14a735
Nanai10a <Nanai10a@outlook.jp>
2021-09-17 04:50:04 +0900

    🐛 fix to do calling usecase's `::handle`.

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -2,7 +2,7 @@ macro_rules! return_inner {
     ($s:ident => use $u:ident,lock $l:ident,ret $r:ident,data $d:ident) => {{
         let guard = $s.$l.lock().await;
 
-        $s.$u($d).await?;
+        $s.$u.handle($d).await?;
         let ret = $s.$r.lock().await.recv().await.unwrap();
 
         drop(guard);
