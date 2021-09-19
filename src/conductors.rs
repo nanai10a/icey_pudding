84a2058ad1066256a6c1d64c2eafbdce3685b91a
Nanai10a <Nanai10a@outlook.jp>
2021-09-19 19:37:06 +0900

    🔊 added logging and do reporing fatal error and few refactering.

--- src/conductors.rs
+++ src/conductors.rs
@@ -1,12 +1,14 @@
+use std::collections::HashMap;
+
 use async_trait::async_trait;
 use serde_json::{json, Number, Value};
-use serenity::builder::CreateMessage;
 use serenity::client::{Context, EventHandler};
 use serenity::model::channel::Message;
 use serenity::model::id::{ChannelId, GuildId, MessageId};
+use tracing::Instrument;
 
 use crate::controllers::serenity::SerenityReturnController;
-use crate::utils::LetChain;
+use crate::utils::{AlsoChain, LetChain};
 
 pub struct Conductor {
     pub contr: SerenityReturnController,
@@ -31,44 +33,106 @@ impl EventHandler for Conductor {
                         #[allow(clippy::unit_arg)]
                         sv.drain(..)
                             .for_each(|v| cm.add_embed(v).let_(::core::mem::drop))
-                            .let_(move |()| {
-                                append_message_reference(cm, msg.id, msg.channel_id, msg.guild_id)
+                            .let_(|()| cm)
+                            .also_(|cm| {
+                                append_message_reference(
+                                    &mut cm.0,
+                                    msg.id,
+                                    msg.channel_id,
+                                    msg.guild_id,
+                                )
                             })
                     })
+                    .instrument(tracing::trace_span!("send_message"))
                     .await,
             Err(e) =>
                 msg.channel_id
                     .send_message(&ctx, |cm| {
-                        cm.content(format!("```{}```", e)).let_(|cm| {
-                            append_message_reference(cm, msg.id, msg.channel_id, msg.guild_id)
+                        cm.content(format!("```{}```", e)).also_(|cm| {
+                            append_message_reference(
+                                &mut cm.0,
+                                msg.id,
+                                msg.channel_id,
+                                msg.guild_id,
+                            )
                         })
                     })
+                    .instrument(tracing::trace_span!("send_message"))
                     .await,
         };
 
+        let e = match res {
+            Ok(o) =>
+                return tracing::info!(
+                    "replied - id {} | channel_id {} | guild_id {} | time {}",
+                    o.id,
+                    o.channel_id,
+                    o.guild_id
+                        .map(|i| i.to_string())
+                        .unwrap_or_else(|| "None".to_string()),
+                    o.timestamp,
+                ),
+            Err(e) => e,
+        };
+
+        tracing::warn!("repling err - {:?}", e);
+
+        let res = msg
+            .channel_id
+            .send_message(ctx, |cm| {
+                cm.content(format!(
+                    "error occurred.
+please send this message to administrator.
+```
+# from_msg
+  - mid  : {}
+  - cid  : {}
+  - gid  : {}
+  - time : {}
+
+# current
+  - time : {}
+
+# err_msg
+{}
+```",
+                    msg.id,
+                    msg.channel_id,
+                    msg.guild_id
+                        .map(|i| i.to_string())
+                        .unwrap_or_else(|| "None".to_string()),
+                    msg.timestamp,
+                    ::chrono::Utc::now(),
+                    e
+                ))
+                .also_(|cm| {
+                    append_message_reference(&mut cm.0, msg.id, msg.channel_id, msg.guild_id)
+                })
+            })
+            .instrument(tracing::trace_span!("send_message"))
+            .await;
+
         match res {
-            Ok(o) => tracing::info!(
-                "replied - id {} | channel_id {} | guild_id {} | time {}",
+            Ok(o) => tracing::warn!(
+                "reported err - id {} | channel_id {} | guild_id {} | time {}",
                 o.id,
                 o.channel_id,
                 o.guild_id
                     .map(|i| i.to_string())
                     .unwrap_or_else(|| "None".to_string()),
-                o.timestamp,
+                o.timestamp
             ),
-            Err(e) => tracing::warn!("repling err - {}", e),
+            Err(e) => tracing::error!("cannot report err - {}", e),
         }
     }
 }
 
-fn append_message_reference<'a, 'b>(
-    raw: &'a mut CreateMessage<'b>,
+fn append_message_reference(
+    raw: &mut HashMap<&str, Value>,
     id: MessageId,
     channel_id: ChannelId,
     guild_id: Option<GuildId>,
-) -> &'a mut CreateMessage<'b> {
-    let CreateMessage(map, ..) = raw;
-
+) {
     let mr = json!({
         "message_id": id,
         "channel_id": channel_id,
@@ -78,7 +142,5 @@ fn append_message_reference<'a, 'b>(
         },
     });
 
-    map.insert("message_reference", mr);
-
-    raw
+    raw.insert("message_reference", mr);
 }
